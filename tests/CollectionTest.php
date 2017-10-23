ba80e37fb27b9450f9085d7774e87bbd81808b32
Fernando <fernandoza@gmail.com>
2017-10-23 13:34:59 -0700

    A couple of methods to test the Collection class.

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -6,11 +6,33 @@
  * Time: 1:13 PM
  */
 
+use PHPUnit\Framework\TestCase;
 
-class CollectionTest extends PHPUnit_Framework_TestCase
+class CollectionTest extends TestCase
 {
-    // methods add, remove, size, contains, isEmpty
-    public function testSomething(){
 
+    public function setUp(){
+        parent::setUp();
+    }
+
+    // methods add, remove, size, contains, isEmpty
+    public function testIsEmpty(){
+        $collection = new Collection();
+        $this->assertTrue(true, $collection->isEmpty());
+        $collection->add('Fer');
+        $this->assetFalse($collection->isEmpty());
+    }
+
+    // it's always a good idea to test 0, 1 and many
+    public function testSize(){
+        $collection = new Collection();
+        $collection->add('one');
+
+        $this->assertSame(1, $collection->size());
+
+        $collection->add(2);
+        $collection->add('three');
+
+        $this->assertSame(3, $collection->size());
     }
 }
