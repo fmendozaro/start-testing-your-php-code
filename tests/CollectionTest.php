bcc38a9bd2a5f219ae06bd550cd04223aa70fac0
Fernando <fernandoza@gmail.com>
2017-10-23 13:19:32 -0700

    Creates the Collection test class.

---
+++ tests/CollectionTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: fer
+ * Date: 10/23/17
+ * Time: 1:13 PM
+ */
+
+
+class CollectionTest extends PHPUnit_Framework_TestCase
+{
+    // methods add, remove, size, contains, isEmpty
+    public function testSomething(){
+
+    }
+}
